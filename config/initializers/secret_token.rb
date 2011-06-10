16e7ad0ae4fc61e95f8c6138460e031bd00f21f9
Francesco Strozzi <francesco.strozzi@gmail.com>
2011-06-10 14:16:03 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BioHub::Application.config.secret_token = '5c2ae8f8fea45f2e87a2ecef8a38dfd346808391cae7440fab4b8a90c1012848dfb1310bb4f16d6e3f3522833426117c6be7180ce4f6d280f104d8c0dd43079b'
