595ab15afd336ae1c72f8ee6aa66acfff269aab9
Francesco Strozzi <francesco.strozzi@gmail.com>
2011-07-04 17:07:54 +0200

    added scheduler_processor to handle incoming requests

--- app/processors/scheduler_processor.rb
+++ app/processors/scheduler_processor.rb
@@ -1,9 +1,13 @@
 class SchedulerProcessor < ApplicationProcessor
 
+  require 'yaml'
+  
   subscribes_to :scheduler
-
+  
   def on_message(message)
-    logger.debug "Processor received: " + message
+    params = YAML.load message
+    task = ScheduledTask.new(params[:classes], params[:name], params[:args][0], params[:args][1])
+    task.launch :delayed => true
   end
   
 end
