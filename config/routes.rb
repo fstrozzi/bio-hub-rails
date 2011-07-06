fde57f2925ffef3c4e0de9d6c4007983c5b4f8f6
Francesco Strozzi <francesco.strozzi@gmail.com>
2011-07-06 15:31:29 +0200

    defined basic route

--- config/routes.rb
+++ config/routes.rb
@@ -48,7 +48,7 @@ BioHub::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+   get "jobs/scheduled"
 
   # See how all your routes lay out with "rake routes"
 
